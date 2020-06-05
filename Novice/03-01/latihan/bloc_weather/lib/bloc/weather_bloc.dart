490f50d3b012689fc8d2e9b2a0d9e3b6e3080263
Nurul30 <nurul1700016056@webmai.uad.ac.id>
2020-06-05 08:51:46 -0700

    update

---
+++ Novice/03-01/latihan/bloc_weather/lib/bloc/weather_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'weather_event.dart';
+part 'weather_state.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  @override
+  WeatherState get initialState => WeatherInitial();
+
+  @override
+  Stream<WeatherState> mapEventToState(
+    WeatherEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
+© 2020 GitHub, In
